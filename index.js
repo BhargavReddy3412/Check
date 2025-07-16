5e84e4af76606cf99c47d1e00e957a5d6aa764c8
Bhargav Reddy <bhargavreddyp948@gmail.com>
2025-07-16 12:55:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'info@expodite.in',
+//     pass: 'uvka smmq jogm ozwe', // replace with app password
+//   },
+// });
+
+app.post('/send-email', (req, res) => {
+  const { firstName, phone, email, company, additionalInfo } = req.body;
+
+  const ipAddress =
+    req.headers['x-forwarded-for']?.split(',')[0] ||
+    req.socket?.remoteAddress ||
+    'IP not available';
+
+
+
+  console.log( firstName, phone, email, company, additionalInfo,ipAddress)
+  return
+  const mailOptions = {
+    from: email,
+    to: 'info@expodite.in',
+    subject: 'New Contact Form Submission',
+    text:
+      `First Name: ${firstName}\n` +
+      `Phone: ${phone}\n` +
+      `Email: ${email}\n` +
+      `Company Name: ${company}\n` +
+      `Additional Information: ${additionalInfo || 'N/A'}\n` +
+      `IP Address: ${ipAddress}`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('success');
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
